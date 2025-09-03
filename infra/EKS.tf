ec61d460738f905dd0b7d4001652a098e9107525
Mataribeiro <mataribeiro@icloud.com>
2025-09-03 08:55:00 -0300

    Update EKS.tf

--- infra/EKS.tf
+++ infra/EKS.tf
@@ -2,9 +2,11 @@ module "eks" {
   source = "terraform-aws-modules/eks/aws"
   version = "21.0.0"
 
-  cluster_name                    = var.cluster_name
-  cluster_version                 = "1.24"
-  cluster_endpoint_public_access  = true
+  eks_cluster = {
+    name                   = var.cluster_name
+    version                = "1.24"
+    endpoint_public_access = true
+  }
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
